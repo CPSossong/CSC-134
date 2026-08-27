0b2d02008dee0b5118cf461026cce85b9ff6d9f3
CPSossong <cpsossong@gmail.com>
2026-08-27 15:55:22 -0400

    Create variables_and_data_types.cpp

---
+++ variables_and_data_types.cpp
@@ -0,0 +1,19 @@
+#include <string>
+using namespace std;
+
+int main()
+{
+    const double SALES_TAX_RATE = 0.07;
+
+    double milesPerGallon;
+
+    char userInput = 'Y';
+
+    bool hasCash = true;
+
+    string favoriteSong = "Clint_Eastwood";
+
+    int birthYear = 1988;
+
+    return 0;
+}
